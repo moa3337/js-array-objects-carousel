fac2e11dcbb5f37a8388f21430d146df6dfa248f
moa3337 <moa-x@hotmail.com>
2023-02-07 19:45:52 +0100

    final commit

--- js/main.js
+++ js/main.js
@@ -16,23 +16,23 @@
 // CREO UN AARAY DI OGETTI, CON FOTO E TITOLO
 const images = [
     {
-        image: 'img/01.webp',
+        image: 'pictures/01.webp',
         title: 'Marvel\'s Spiderman Miles Morale',
         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
     }, {
-        image: 'img/02.webp',
+        image: 'pictures/02.webp',
         title: 'Ratchet & Clank: Rift Apart',
         text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
     }, {
-        image: 'img/03.webp',
+        image: 'pictures/03.webp',
         title: 'Fortnite',
         text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
     }, {
-        image: 'img/04.webp',
+        image: 'pictures/04.webp',
         title: 'Stray',
         text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
     }, {
-        image: 'img/05.webp',
+        image: 'pictures/05.webp',
         title: "Marvel's Avengers",
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
@@ -51,7 +51,24 @@ const btnNextEl = document.getElementById("next");
 
 // FUNZIONE PER IL PER LO SCAMBIO DEGLI ELEMENTI:
 // "img", "title" e "description"
-function name(params) {
-
-
+function updateCarousel() {
+    const currentImage = images[currentImg];
+    imageEl.src = currentImage.image;
+    titleEl.innerText = currentImage.title;
+    descriptionEl.innerText = currentImage.text;
 }
+
+// FUNZIONE PER L'EVENTO AL "click" DEL BOTTONE "prev"
+btnPrevEl.addEventListener('click', () => {
+    currentImg =
+        currentImg === 0 ? images.length - 1 : currentImg - 1;
+    updateCarousel();
+});
+
+// FUNZIONE PER L'EVENTO AL "click" DEL BOTTONE "next"
+btnNextEl.addEventListener('click', () => {
+    currentImg = (currentImg + 1) % images.length;
+    updateCarousel();
+});
+
+updateCarousel();
