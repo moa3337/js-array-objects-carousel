ead3308d81de1143e883c178a021d8e2d516ced0
moa3337 <moa-x@hotmail.com>
2023-02-07 16:08:25 +0100

    setting cod

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/*
+ Dato un array di oggetti letterali con: immagine, titolo, descrizione. 
+ Creare un carosello.
+ Creare prima il markup statico: costruire il container e inserire
+ l'immagine grande in modo da poter stilare lo slider. 
+ Poi rimuovere i contenuti statici e usare l'array di oggetti letterali
+ per popolare dinamicamente il carosello.
+ Al click dell'utente sulle frecce verso sinistra o destra, l'immagine
+ attiva diventerà visibile e dovremo aggiungervi titolo e testo.
+ Aggiungere il **ciclo infinito** del carosello. Ovvero se la miniatura
+ attiva è la prima e l'utente clicca la freccia verso destra, la miniatura
+ che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura
+ se l'utente clicca la freccia verso sinistra.
+*/
