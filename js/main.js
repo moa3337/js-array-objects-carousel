347de1e6d0deb32ab1312d1ec3cb952b33b9c1a8
moa3337 <moa-x@hotmail.com>
2023-02-07 16:40:13 +0100

    markup statico

--- js/main.js
+++ js/main.js
@@ -12,3 +12,46 @@
  che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura
  se l'utente clicca la freccia verso sinistra.
 */
+
+// CREO UN AARAY DI OGETTI, CON FOTO E TITOLO
+const images = [
+    {
+        image: 'img/01.webp',
+        title: 'Marvel\'s Spiderman Miles Morale',
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    }, {
+        image: 'img/02.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    }, {
+        image: 'img/03.webp',
+        title: 'Fortnite',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    }, {
+        image: 'img/04.webp',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    }, {
+        image: 'img/05.webp',
+        title: "Marvel's Avengers",
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+    }
+];
+
+// DICHIARO UNA VARIABILE PER L'IMMAGINE CORRENTE
+let currentImg = 0;
+
+// AGGANCIO AGLI "id" PER IMMAGINE E TITOLO DAL HTML
+const imageEl = document.getElementById("image");
+const titleEl = document.getElementById("title");
+const descriptionEl = document.getElementById("description");
+// AGGANCIO ANCHE AGLI "id" DEI BOTTONI DAL HTML
+const btnPrevEl = document.getElementById("prev");
+const btnNextEl = document.getElementById("next");
+
+// FUNZIONE PER IL PER LO SCAMBIO DEGLI ELEMENTI:
+// "img", "title" e "description"
+function name(params) {
+
+
+}
